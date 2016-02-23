ca20958feb7beff865f11196bde639b7a4ddf266
tkay <aruntknambiar@gmail.com>
2016-02-23 20:28:54 +0530

    dependencies to latest versions

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'mwc:extensions',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'MWC compiler extensions',
   git: "https://github.com/meteorwebcomponents/extensions.git",
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.use('mwc:ecmascript@0.0.3','server',{weak:true});
+  api.use('mwc:ecmascript@0.0.5','server',{weak:true});
   api.versionsFrom('1.0');
   api.addFiles(['extensions.js'],'server');
   api.export('MWCExtend','server');
