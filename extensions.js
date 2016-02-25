ee11a576178b3dab5a65bd8dec50eadb157c918d
tkay <aruntknambiar@gmail.com>
2016-02-25 17:23:54 +0530

    options enabled. using ecma 1.0.4

--- extensions.js
+++ extensions.js
@@ -1,8 +1,9 @@
-// Write your package code here!
-//
-MWCExtend = function(html){
+var fs = Npm.require("fs");
+MWCExtend = function(html,extensions){
+extensions = extensions || {};
 if(typeof(Package.mwc_ecmascript) !== 'undefined'){
- html = Package.mwc_ecmascript.MWCEcma.compile(html);
+ html = Package.mwc_ecmascript.MWCEcma.compile(html,extensions['ecmascript']);
 }
+fs.writeFile('.mwclogs/extensions.txt',JSON.stringify(Package));
 return html;
 }
--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
   name: 'mwc:extensions',
-  version: '1.0.2',
+  version: '1.0.4',
   summary: 'MWC compiler extensions',
   git: "https://github.com/meteorwebcomponents/extensions.git",
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.use('mwc:ecmascript@1.0.2','server',{weak:true});
+  api.use('mwc:ecmascript','server',{weak:true});
+  api.use('underscore',['server']);
   api.versionsFrom('1.0');
   api.addFiles(['extensions.js'],'server');
   api.export('MWCExtend','server');
